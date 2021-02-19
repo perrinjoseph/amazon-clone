c06346a4ad0c6cf209f7629d0c34d3416c7a265b
perrinjoseph <perrinjoseph1998@gmail.com>
2021-02-19 03:19:41 -0500

    My first Commit

---
+++ src/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./Home.css";
+import Product from './Product';
+
+function Home() {
+    return (
+        <div className = "home" >
+            <div className="home__container">
+                <img className="home__image" src="https://store-images.s-microsoft.com/image/apps.16285.14618985536919905.552c0017-6644-49a8-8467-8f7b34ce0428.30ad6b05-16d9-4d5e-a242-43107708a16a?mode=scale&q=90&h=1080&w=1920" alt="amazon Banner"/>
+                <div className="home__row">
+                    <Product/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDDsItpJpo60ZoJFZwLMWc2Vj7tHyTvJ68",
+    authDomain: "clone-1c5c4.firebaseapp.com",
+    projectId: "clone-1c5c4",
+    storageBucket: "clone-1c5c4.appspot.com",
+    messagingSenderId: "990527761848",
+    appId: "1:990527761848:web:5916f9b228f5846d15334d",
+    measurementId: "G-9789CYJH3V"
+  };
